7707d7124b89d29eeba64ca646b90f5e46dcca6d
navinpai <lifeofnavin@gmail.com>
2018-12-15 01:25:13 +0530

    Add additional formatting styles and comments

--- 1_hello.rs
+++ 1_hello.rs
@@ -13,9 +13,22 @@ fn main(){
              verb="jumps over");
 
     let x = 5;
-    println!("value is {}", x)
+    println!("value is {}", x);
+
+    // :b converts to binary 
+    println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
 
     // println == print but with newline
     // eprintln == eprint but with newline
     // eprint == print but writes out to stderr instead of stdout
+    
+    let pi = 3.141592;
+    println!("Value of pi {:0.3}", pi);
+
+    // This generates a warning saying Deadcode unused. To use uncomment line below
+    // #[allow(dead_code)]
+    // struct Structure(i32);
+    
+    // #[allow(unused_variables)] or convert var to _y
+    // let y = 7;
 }
